dce958e6fe5388271a4ba4f8c30e69a4dbd2c9eb
kchen29 <kchen29@stuy.edu>
2018-02-15 11:34:32 -0500

    split test and fancy image

--- main.lisp
+++ main.lisp
@@ -1,12 +1,10 @@
 (eval-when (:compile-toplevel :load-toplevel)
   (load "draw"))
 
-(defun main (a-size filename)
-  "Tests matrix multiplication, making an A-SIZE by A-SIZE image. Outputs to FILENAME."
-  (let* ((dimensions (list a-size a-size))
-         (screen (make-array dimensions :initial-element '(0 0 0)))
-         (edge-list (make-matrix 4 0))
-         (transform (make-matrix)))
+(defun main-test ()
+  "Tests matrix stuff."
+  (let ((edge-list (make-matrix 4 0))
+        (transform (make-matrix)))
     (format t "transform matrix:")
     (print-matrix transform)
 
@@ -17,7 +15,6 @@
     (format t "add an edge to the edge matrix")
     (add-edge edge-list 0 250 0 499 0 0)
     (print-matrix edge-list)
-    (draw-lines edge-list screen '(255 0 0))
     
     (format t "multiply the edge matrix with the transform matrix")
     (setf edge-list (matrix-multiply transform edge-list))
@@ -29,10 +26,43 @@
 
     (format t "apply the transform to the edge list")
     (setf edge-list (matrix-multiply transform edge-list))
-    (print-matrix edge-list)
-    (draw-lines edge-list screen '(255 0 0))
+    (print-matrix edge-list)))
+
+
+(defun draw-transform-draw (transform edge-list screen color)
+  (draw-lines edge-list screen color)
+  (setf edge-list (matrix-multiply transform edge-list))
+  (draw-lines edge-list screen color))
+
+(defmacro add-edges (matrix edges)
+  "Add multiple edges to the matrix."
+  `(progn ,@(loop for edge in edges
+               collect `(add-edge ,matrix
+                                  ,(first edge) ,(second edge) 0
+                                  ,(third edge) ,(fourth edge) 0))))
+
+(defun main (a-size filename)
+  "Make fancy image, making an A-SIZE by A-SIZE image. Outputs to FILENAME."
+  (let* ((dimensions (list a-size a-size))
+         (half (/ a-size 2))
+         (full (1- a-size))
+         (screen (make-array dimensions :initial-element '(0 0 0)))
+         (color '(0 255 0))
+         (edges (make-matrix 4 0))
+         (transform (make-matrix)))
+    ;;make transform rotate 22.5 degrees from the point (250, 250)
+    (to-identity transform)
+    
+    (add-edges edges ((0 0 full full)
+                      (0 0 full half)
+                      (full full 0 half)))
+  
+    (draw-lines edges screen color)
+
     
     (write-ppm filename dimensions screen)
     (display filename)))
 
+(main-test)
 (main 500 "output.ppm")
+
